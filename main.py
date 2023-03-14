5ab81a21f2065f3550e3feb805e796d42a1f9a57
GenFuruya <nehahako553@gmail.com>
2023-03-15 01:55:31 +0900

    add main,func, table.csv and manual.txt

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import random
+import func
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+with open("./env.txt", "r", encoding='utf-8') as f:
+    token_list = [s.strip() for s in f.readlines()]
+TOKEN = os.getenv("TOKEN")
+CHANNELID = 787843827779108867
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        #オウム
+        channel = client.get_channel(CHANNELID)
+        # await channel.send(message.content)
+        # print(message.content)
+
+        # if message.content == "random":
+        #     text = func.randomWheel()
+        #     await channel.send(text)
+        #     print(text)
+        t = func.createResponse(message.content)
+        await channel.send(t)
+        print(t)
+
+client.run(TOKEN)
+
